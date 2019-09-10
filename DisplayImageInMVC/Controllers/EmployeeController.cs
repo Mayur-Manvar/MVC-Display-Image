5f38aa7c9f755877068895ec383f18971d7e4a1d
Mayur-Manvar <mayurpmanvar@gmail.com>
2019-09-10 22:58:55 +0530

    Display Images on MVC

---
+++ DisplayImageInMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DisplayImageInMVC.Models;
+
+namespace DisplayImageInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(int id)
+        {
+            EmployeeEntities employeeEntities = new EmployeeEntities();
+            tblEmployee employee = employeeEntities.tblEmployees.Single(x => x.Id == id);
+
+            return View(employee);
+        }
+    }
+}
---
+++ DisplayImageInMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DisplayImageInMVC.Startup))]
+namespace DisplayImageInMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
