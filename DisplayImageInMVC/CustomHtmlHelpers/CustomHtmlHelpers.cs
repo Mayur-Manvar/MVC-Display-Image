9c65d944f3b7c2c4ad0d7870d4ef01bfa5d91aca
Mayur-Manvar <mayurpmanvar@gmail.com>
2019-09-10 23:25:45 +0530

    Create Custom HTML Helpers

---
+++ DisplayImageInMVC/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisplayImageInMVC.CustomHtmlHelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string path, string alternateText)
+        {
+            TagBuilder tagBuilder = new TagBuilder("img");
+
+            tagBuilder.Attributes.Add("src", VirtualPathUtility.ToAbsolute(path));
+            tagBuilder.Attributes.Add("alt", alternateText);
+
+            return new HtmlString(tagBuilder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
